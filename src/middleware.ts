b700225d0e05c20413ca21786c77e223ccb5a185
Zane Lucas <beinginzane@being.inzane>
2026-08-27 21:42:18 +0530

    add www to non-www redirect middleware

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get("host") || "";
+
+  if (host.startsWith("www.")) {
+    const url = request.nextUrl.clone();
+    url.host = host.replace("www.", "");
+    return NextResponse.redirect(url, 308);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo-dark.png).*)"],
+};
